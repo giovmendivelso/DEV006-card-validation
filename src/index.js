17d5b01dd916dbd4048435863e40b4d9673025cc
Giovanna Mendivelso <127072876+giovmendivelso@users.noreply.github.com>
2023-04-18 19:59:02 -0500

    Trabajando en el proyecto.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,143 @@
 import validator from './validator.js';
 
 console.log(validator);
+const tarjetadecredito = document.getElementById("numero")
+const finalizar = document.getElementById("Terminar")
+
+
+
+
+finalizar.addEventListener( )
+
+const nombre = document.getElementById('inputname');
+const numeroTarjetaInput = document.getElementById('numero-tarjeta');
+const mesInput = document.getElementById("inputmes");
+//const yearInput = document.getElementById("inputano");
+//const cvvInput = document.getElementById("inputcvc");
+const verificarTarjetaButton = document.getElementById('verificar-tarjeta');
+
+// obtener los elementos para la tarjeta
+const parrafo = document.getElementById('tarjeta-numero');
+
+const inputNombre = document.querySelector("#inputname");
+const inputTarjeta = document.querySelector("#numero-tarjeta");
+const inputMes = document.querySelector("#inputmes");
+const inputYear = document.querySelector("#inputano");
+const inputCvv = document.querySelector("#inputcvc");
+
+
+const cardName = document.querySelector("#tarjeta-nombre");
+const cardNumber = document.querySelector("#tarjeta-numero");
+const cardMes = document.querySelector("#tarjeta-mes");
+const cardYear = document.querySelector("#tarjeta-year"); 
+const cardCvv = document.querySelector("#tarjeta-cvv");
+
+
+// Darle funcion a la tarjeta   funcion para el nombre    
+inputNombre.addEventListener("input", () => { // detecta cuando el usuarios hace un evento en mi input y le pasamos una funcion como 2 parametro
+    cardName.innerText = inputNombre.value;
+    if(inputNombre.value.length === 0) {
+      cardName.innerText ="INGRESE NOMBRE"
+    }
+  
+  });
+  
+  nombre.addEventListener("input", function(){
+    let soloNom = this.value;
+    soloNom = soloNom.replace(/[^a-zA-Z]/g," ")
+    this.value = soloNom;
+  });
+  
+  
+  // ingresar dentro de mi tarjeta el maskify y solo campos numericos en input de numero de tarjeta
+  inputTarjeta.addEventListener("input", () => {
+    cardNumber.innerText = inputTarjeta.value;
+  
+    
+    
+    
+    const originalTexto = parrafo.textContent;
+    const maskedTexto = validator.maskify(originalTexto);  
+    parrafo.textContent = maskedTexto;
+    
+    if(inputTarjeta.value.length === 0){
+      cardNumber.innerText ="0000 0000 0000 0000"
+    }
+  });
+  
+  numeroTarjetaInput.addEventListener("input", function(){
+    let soloNum = this.value;
+    soloNum = soloNum.replace(/[^0-9]/g, "")
+    this.value = soloNum;
+  });
+  
+  
+  
+  // input mes
+  inputMes.addEventListener("input", () => {
+    cardMes.innerText = inputMes.value;
+    if(inputMes.value.length === 0) {
+      cardMes.innerText ="00"
+    }
+    
+  })
+  
+  mesInput.addEventListener("input", function(){
+    let soloNu = this.value;
+    soloNu = soloNu.replace()
+    this.value = soloNu;
+  });
+  
+  
+  
+  // input año
+  inputYear.addEventListener("input", () => {
+    cardYear.innerText = inputYear.value;
+    if(inputYear.value.length === 0) {
+      cardYear.innerText ="00"
+    }
+  })
+  
+  
+  
+  //input cvv
+  inputCvv.addEventListener("input", () => {
+    cardCvv.innerText = inputCvv.value;
+    if(inputCvv.value.length === 0) {
+      cardCvv.innerText ="000"
+    }
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // Evento de escuchar o llamar al botón para validar tarjeta
+  verificarTarjetaButton.addEventListener('click', function() 
+  {
+    const esValida = validator.isValid(numeroTarjetaInput.value);
+    if(numeroTarjetaInput.value === "" || nombre.value === "" )
+    {
+      alert('Rellene todos los campos');
+    }
+    else{
+      if (esValida) 
+      {        
+        alert('Tarjeta Valida!');
+        document.getElementById("gracias").style.display ='block' 
+        document.getElementById("contenedor").style.display ='none' // debo cambiar el alert por un mensaje en pantalla (con otro html?)
+        document.getElementById("tarjeta").style.display ="none"
+      } 
+      else 
+      {
+        alert('Ingrese un número de tarjeta valido');
+      }
+    }
+  }
+    ,);
+  
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,37 @@
-const validator = {
-  // ...
+const validator = {//algoritmo de Lunh //
+  isvalid: function (tarjetadecredito){
+    let multiplicar = 1
+let suma = 0; /*la suma va a partir de 0*/
+    for(let i = tarjetadecredito.length -1; i >= 0; i--){ 
+       // Recorrer los dígitos de la tarjeta de crédito de derecha a izquierda // leng cuenta posiciones que tiene//
+    let numeros = parseInt(tarjetadecredito.charArt(i))
+    //ChatAT accede a un caracter//
+
+      if (multiplicar % 2 === 0){
+        // aqui verifico si el multiplicador es uno o dos si es 1 no entra al if//
+
+
+     
+      numeros *=2
+      //si es de la posicion 2 lo multiplica por 2//
+      if (numeros > 9){
+        // Si el resultado de la multiplicación es mayor a 9, sumar los dígitos del número resultante//
+        numeros -= 9;
+        //si a la suma le resto y 9 queda lo mismo//
+      } 
+    }
+    multiplicar = multiplicar === 1 ? 2 : 1;
+     // Cambiar el multiplicador para alternar entre 1 y 2//
+    summa += numeros; 
+    // Sumar el dígito actual a la suma total//   
+    }
+    return suma % 10 === 0;
+    // La tarjeta de crédito es válida si la suma total es un múltiplo de 10//
+  },
 };
+
+maskify: function (input) {
+  return input.slice(0, -4).replace(/./g, "#") + input.slice(-4); // maskify
+},
 
 export default validator;
